ca0ef9a764a30e03525db56af6bb9abc24b3bd41
Najam Syed <najam.r.syed@gmail.com>
2020-04-09 11:48:34 -0400

    wip

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 import urllib
 
 
-def parse_book_url(url, title, author, format_='pdf'):
+def parse_book_url(url, title, author, format_="pdf"):
     """
     Given the original book URL, get the download URL and human-friendly
     local filename for the given format ('pdf' or 'epub').
@@ -23,20 +23,18 @@ def parse_book_url(url, title, author, format_='pdf'):
     # on `format_`), and add format extension.
     download_url = urllib.parse.unquote(url)
     if format_ == "pdf":
-        download_url = download_url.replace('book', 'content/' + format_)
+        download_url = download_url.replace("book", "content/" + format_)
     elif format_ == "epub":
-        download_url = download_url.replace('book', 'download/' + format_)
+        download_url = download_url.replace("book", "download/" + format_)
     else:
         raise ValueError("Unsupported format: {}".format(format_))
-    download_url += '.' + format_
+    download_url += "." + format_
 
     original_fname = os.path.split(download_url)[1]
 
     # Replace desired characters for human-friendly local filename.
     char_replacements = {
-        ',': '-',
-        '.': '',
-        '/': ' ',
+        "/": "_",
     }
 
     new_fname = [
@@ -44,12 +42,12 @@ def parse_book_url(url, title, author, format_='pdf'):
         author.translate(str.maketrans(char_replacements)),
         original_fname
     ]
-    new_fname = ' - '.join(new_fname)
+    new_fname = " - ".join(new_fname)
 
     return download_url, new_fname
 
 
-def get_books(book_table):
+def book_info_from_table(book_table):
     """
     Yield relevant book info for each book in the table.
     """
@@ -59,14 +57,55 @@ def get_books(book_table):
         "Book Title", "Author", "English Package Name", "OpenURL"
     ]
 
-    for title, author, pkg, url in book_table[columns].values():
+    for title, author, pkg, url in book_table[columns].values:
         yield {
             "title": title,
             "author": author,
-            "pkg": pkg,
+            "pkg": pkg.replace(" ", "_").replace(",", ""),
             "url": url
         }
 
+
+def download_book(book, dst_dir, get_epub=False):
+    book_dir = dst_dir / book["pkg"]
+    book_dir.mkdir(exist_ok=True)
+
+    success = {
+        "pdf": False,
+        "epub": None
+    }
+
+    request = requests.get(book["url"])
+
+    pdf_download_url, pdf_fname = parse_book_url(
+        request.url, book["title"], book["author"], "pdf"
+    )
+    pdf_fpath = book_dir / pdf_fname
+    pdf_request = requests.get(pdf_download_url, allow_redirects=True)
+
+    if pdf_status_code == 200:
+        with open(str(pdf_fpath), "wb") as f:
+            ret = f.write(pdf_request.content)
+            success["pdf"] = ret > 0
+
+    if get_epub:
+        success["epub"] = False
+        epub_download_url, epub_fname = parse_book_url(
+            request.url, book["title"], book["author"], "epub"
+        )
+        epub_fpath = book_dir / epub_fname
+        epub_request = requests.get(epub_download_url, allow_redirects=True)
+
+        if epub_request.status_code == 200:
+            with open(str(epub_fpath), "wb") as f:
+                ret = f.write(epub_request.content)
+                success["epub"] = ret > 0
+    book["success"] = success
+
+
+def download_books(dst_dir, book_table, get_epub=False, max_workers=8):
+    books = list(book_info_from_table(book_table))
+    download_book(books[0], dst_dir, get_epub)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -93,28 +132,4 @@ if __name__ == "__main__":
     # Save table.
     book_table.to_excel(str(table_path))
 
-
-if __name__ == "false":
-        new_folder = os.path.join(folder, pk_name)
-
-        if not os.path.exists(new_folder):
-            os.mkdir(new_folder)
-
-        r = requests.get(url)
-
-        # Download pdf version.
-        pdf_download_url, pdf_fname = parse_book_url(r.url, title, author, 'pdf')
-        pdf_fpath = os.path.join(new_folder, pdf_fname)
-
-        pdf_request = requests.get(pdf_download_url, allow_redirects=True)
-        with open(pdf_fpath, 'wb') as f:
-            f.write(pdf_request.content)
-
-        #download epub version too if exists
-        epub_download_url, epub_fname = parse_book_url(r.url, title, author, 'epub')
-        epub_fpath = os.path.join(new_folder, epub_fname)
-
-        epub_request = requests.get(epub_download_url, allow_redirects=True)
-        if epub_request.status_code == 200:
-            with open(epub_fpath, 'wb') as f:
-                f.write(epub_request.content)
+    download_books(dst_dir, book_table, get_epub=args["epub"])
